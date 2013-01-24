112f780e85f3f3e81ce05681e619b8570b22facb
Agaricus <agaricusb@yahoo.com>
2013-01-23 22:25:08 -0800

    Add classloader inheritance provider
    Allows inheritance to be loaded through URLClassLoader, as an alternative
    to JarFile/Jar with JarInheritanceProvider. This is how CB loads plugins, and
    it may avoid some of the idiosyncrasies in loading the jar directly as a zip -
    for example https://github.com/MinecraftPortCentral/MCPC-Plus/issues/51
    java.util.zip.ZipException: duplicate entry (CB can load this plugin just fine)
    
    Also allows more convenient remapping in PluginClassLoader, since the
    URLClassLoader object is readily available

---
+++ src/main/java/net/md_5/specialsource/URLClassLoaderInheritanceProvider.java
@@ -0,0 +1,56 @@
+package net.md_5.specialsource;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Lookup inheritance from a class in a given URLClassLoader.
+ */
+public class URLClassLoaderInheritanceProvider implements IInheritanceProvider {
+    private final URLClassLoader classLoader;
+
+    public URLClassLoaderInheritanceProvider(URLClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<String> getParents(String owner) {
+        try {
+            String ownerInternalName = owner.replace('.', '/'); // TODO: abstract out
+            URL url = classLoader.findResource(ownerInternalName);
+            if (url == null) {
+                return null;
+            }
+
+            InputStream inputStream = url.openStream();
+            if (inputStream == null) {
+                return null;
+            }
+
+            // TODO: refactor
+
+            ClassReader cr = new ClassReader(inputStream);
+            ClassNode node = new ClassNode();
+            cr.accept(node, 0);
+
+            List<String> parents = new ArrayList<String>();
+
+            for (String iface : (List<String>) node.interfaces) {
+                parents.add(iface);
+            }
+            parents.add(node.superName);
+
+            return parents;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+}
