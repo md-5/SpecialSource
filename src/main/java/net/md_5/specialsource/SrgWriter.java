e99d3c7ef4cb478953fb2fa8ba60497f66f8bdf4
Agaricus <agaricusb@yahoo.com>
2013-02-27 21:18:22 -0800

    Update date to 2012-2013

--- src/main/java/net/md_5/specialsource/SrgWriter.java
+++ src/main/java/net/md_5/specialsource/SrgWriter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, md_5. All rights reserved.
+ * Copyright (c) 2012-2013, md_5. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -40,7 +40,7 @@ public class SrgWriter implements ISrgWriter {
 
     private static final String HEADER = ""
             + "# THESE ARE AUTOMATICALLY GENERATED MAPPINGS BETWEEN {0} and {1}\n"
-            + "# THEY WERE GENERATED ON {2} USING Special Source (c) md_5 2012.\n"
+            + "# THEY WERE GENERATED ON {2} USING Special Source (c) md_5 2012-2013.\n"
             + "# PLEASE DO NOT REMOVE THIS HEADER!\n";
     private List<String> lines;
     private PrintWriter out;
